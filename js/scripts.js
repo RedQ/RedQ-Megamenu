d48f42ad7712526a1576be75af72facf16a52a93
Faysal haque <faysal.haque@gmail.com>
2014-06-04 15:13:02 +0600

    remove icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,29 @@
 (function($) {
-    var shrinkHeader = 300;
+	"use strict";
+
+    // var shrinkHeader = 300;
 
 
-    // $('#header').next().html('<div style="height: 103px"></div>');
-    var shrink = $('#header').next();
+    // // $('#header').next().html('<div style="height: 103px"></div>');
+    // var shrink = $('#header').next();
 
-    	shrink.css('margin-top', 103);
+    // 	shrink.css('margin-top', 103);
 
-    // $('.shrinkHeader').css('margin-top', 103);
+    // // $('.shrinkHeader').css('margin-top', 103);
 
-    $(window).scroll(function() {
-        var scroll = getCurrentScroll();
-        if (scroll >= shrinkHeader) {
-            $('.header').addClass('shrink');
-        } else {
-            $('.header').removeClass('shrink');
+    // $(window).scroll(function() {
+    //     var scroll = getCurrentScroll();
+    //     if (scroll >= shrinkHeader) {
+    //         $('.header').addClass('shrink');
+    //     } else {
+    //         $('.header').removeClass('shrink');
 
-        }
-    });
+    //     }
+    // });
 
-    function getCurrentScroll() {
-        return window.pageYOffset || document.documentElement.scrollTop;
-    }
+    // function getCurrentScroll() {
+    //     return window.pageYOffset || document.documentElement.scrollTop;
+    // }
 })(jQuery);
 
 (function($) {
@@ -35,19 +37,19 @@
 			RedQ.redQ_nav_hover();
 		},
 
-		redQ_nav_hover : function() {
-			$('.dropdown').on('mouseenter', function() {
-				var self = $(this);
+		// redQ_nav_hover : function() {
+		// 	$('.dropdown').on('mouseenter', function() {
+		// 		var self = $(this);
 
-				self.find('.dropdown-menu').stop().slideDown(300, easing);
-			});
+		// 		self.find('.dropdown-menu').stop().slideDown(300, easing);
+		// 	});
 
-			$('.dropdown').on('mouseleave', function() {
-				var self = $(this);
+		// 	$('.dropdown').on('mouseleave', function() {
+		// 		var self = $(this);
 
-				self.find('.dropdown-menu').stop().slideUp(300, easing);
-			});
-		},
+		// 		self.find('.dropdown-menu').stop().slideUp(300, easing);
+		// 	});
+		// },
 
 
 	};
