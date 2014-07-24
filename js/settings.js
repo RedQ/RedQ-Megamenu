870c2e2d1d5532973351392d0ab3c8797f5a4523
Faysal haque <faysal.haque@gmail.com>
2014-07-25 04:41:45 +0600

    edit title

--- js/settings.js
+++ js/settings.js
@@ -1,51 +1,51 @@
 (function($) {
-	"use strict";
+    "use strict";
 
-	var redQ_settings = {
+    var redQ_settings = {
 
-		redQ_settings_init : function() {
-			redQ_settings.menu_align();
-			redQ_settings.menu_icons();
-			redQ_settings.menu_animation();
-			redQ_settings.menu_color_skins();
-		},
+        redQ_settings_init: function() {
+            redQ_settings.menu_align();
+            redQ_settings.menu_icons();
+            redQ_settings.menu_animation();
+            redQ_settings.menu_color_skins();
+        },
 
 
-		menu_align : function() {
-			$('#menu-align').on('change', '[name="change-align"]', function() {
-				var name = $(this).val();
+        menu_align: function() {
+            $('#menu-align').on('change', '[name="change-align"]', function() {
+                var name = $(this).val();
 
-				$('#mega-menu.primary-menu').removeClass('align-left align-right align-center').addClass(name);
+                $('#mega-menu.primary-menu').removeClass('align-left align-right align-center').addClass(name);
 
-			});
-		},
-		menu_icons : function() {
-			$('#menu-icons').on('change', '[name="change-icons"]', function() {
-				var name = $(this).val();
+            });
+        },
+        menu_icons: function() {
+            $('#menu-icons').on('change', '[name="change-icons"]', function() {
+                var name = $(this).val();
 
-				$('#mega-menu.primary-menu').removeClass('icons-no icons-left icons-right icons-top').addClass(name);
+                $('#mega-menu.primary-menu').removeClass('icons-no icons-left icons-right icons-top').addClass(name);
 
-			});
-		},
-		menu_animation : function() {
-			$('#menu-animation').on('change', '[name="change-animation"]', function() {
-				var name = $(this).val();
+            });
+        },
+        menu_animation: function() {
+            $('#menu-animation').on('change', '[name="change-animation"]', function() {
+                var name = $(this).val();
 
-				$('#mega-menu.primary-menu').removeClass('animate-null fadeInUp fadeInDown bounceInUp flipInX flipInY pulse zoomIn lightSpeedIn').addClass(name);
+                $('#mega-menu.primary-menu').removeClass('animate-null fadeInUp fadeInDown bounceInUp flipInX flipInY pulse zoomIn lightSpeedIn').addClass(name);
 
-			});
-		},
-		menu_color_skins : function() {
-			$('#color-skins').on('change', '[name="change-skins"]', function() {
-				var name = $(this).val();
+            });
+        },
+        menu_color_skins: function() {
+            $('#color-skins').on('change', '[name="change-skins"]', function() {
+                var name = $(this).val();
 
-				$('#mega-menu.primary-menu').removeClass('default-skin green-sea-skin pomegranate-skin silver-skin pumpkin-skin wisteria-skin orange-skin nephritis-skin belize-hole-skin midnight-blue-skin asbestos-skin').addClass(name);
+                $('#mega-menu.primary-menu').removeClass('default-skin green-sea-skin pomegranate-skin silver-skin pumpkin-skin wisteria-skin orange-skin nephritis-skin belize-hole-skin midnight-blue-skin asbestos-skin').addClass(name);
 
-			});
-		},
+            });
+        },
 
-	};
+    };
 
-	redQ_settings.redQ_settings_init();
+    redQ_settings.redQ_settings_init();
 
 })(jQuery);
